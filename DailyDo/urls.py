3e20b05c047399e8fcbc2013bf9a7dc701120123
Anshuman Rath <anshumanrath030@gmail.com>
2024-07-31 15:39:21 +0530

    ..

--- DailyDo/urls.py
+++ DailyDo/urls.py
@@ -26,8 +26,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(urls)),
     path('api/',include(apiurls)),
+    path('',include(urls)),
+    
 ]
 urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,2 @@
-pip3 install -r requirements.txt
-
-
-#!/bin/bash
-
-# Check Python version
-# python3 --version
-
-# Install pip for Python 3
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# python3 get-pip.py
-
-# Install required Python packages
-
-# Continue with your build steps
-# python3 manage.py collectstatic --noinput
-# python3 manage.py migrate
+pip install -r requirements.txt
+python3 manage.py collectstatic
