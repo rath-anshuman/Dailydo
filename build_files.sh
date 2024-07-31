df69cb1b6bc5922fa374b8fdc40f53119d9a6adc
Anshuman Rath <anshumanrath030@gmail.com>
2024-07-31 15:02:02 +0530

    .

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
 
+# Ensure that pip is installed
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+
+# Install required Python packages
 pip install -r requirements.txt
-python3 manage.py collectstatic
+
+# Continue with your build steps
+python manage.py migrate
