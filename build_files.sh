f159dbb407fe28e35162eb3b3b12033e4026abb2
Anshuman Rath <anshumanrath030@gmail.com>
2024-07-31 14:59:00 +0530

    .

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 pip install -r requirements.txt
 python3 manage.py collectstatic
